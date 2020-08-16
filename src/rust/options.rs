cf69a3c33ac118cdb02fbabe41d11a423c171a39
Joel Mueller <joel.mueller@gmail.com>
2020-08-16 03:02:06 -0500

    Updating rand

--- src/rust/options.rs
+++ src/rust/options.rs
@@ -12,10 +12,7 @@ impl Options {
             save_dir: String::new(),
             save_name: String::new(),
             log_instructions: false,
-            rand_seed: [
-                0xa8, 0x5b, 0xb3, 0x37, 0x1b, 0x36, 0xc8, 0x11, 0x72, 0x12, 0xfa, 0xd7, 0xea, 0xde,
-                0xda, 0x8b,
-            ],
+            rand_seed: [0; 16],
         }
     }
 }
